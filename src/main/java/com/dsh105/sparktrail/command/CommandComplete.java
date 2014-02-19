337dc4b43ed438db616672bedd99eeb868dc3208
DSH105 <jason.chirgwin@icloud.com>
2014-02-19 19:50:17 +1100

    Add command completion for sounds

--- src/main/java/com/dsh105/sparktrail/command/CommandComplete.java
+++ src/main/java/com/dsh105/sparktrail/command/CommandComplete.java
@@ -1,6 +1,8 @@
 package com.dsh105.sparktrail.command;
 
+import com.dsh105.dshutils.util.StringUtil;
 import com.dsh105.sparktrail.SparkTrailPlugin;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -16,7 +18,15 @@ public class CommandComplete implements TabCompleter {
         List<String> list = new ArrayList<String>();
         String cmdString = SparkTrailPlugin.getInstance().cmdString;
         if (cmd.getName().equalsIgnoreCase(cmdString)) {
-
+            if (args.length > 0) {
+                if (args[0].equalsIgnoreCase("sound")) {
+                    for (Sound s : Sound.values()) {
+                        if (s.toString().toLowerCase().startsWith(args[args.length - 1])) {
+                            list.add(s.toString().toLowerCase());
+                        }
+                    }
+                }
+            }
         }
         return list;
     }
